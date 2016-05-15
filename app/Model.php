5dd05430a16f934b48f73940b1943f557f787e30
cbd <wwwicbd@gmail.com>
2016-05-15 13:06:19 +0800

    add index.php

--- app/Model.php
+++ app/Model.php
@@ -5,7 +5,7 @@
  * Date: 16/5/15
  * Time: 上午11:10
  */
-include "redis-conf.php";
+include "conf/redis-conf.php";
 
 $redis = new Redis();
 $redis->connect($config['redis_local']['host'], $config['redis_local']['post'], 3);
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cbd
+ * Date: 16/5/15
+ * Time: 下午1:03
+ */
+
+include "app/Controller.php";
