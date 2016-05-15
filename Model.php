c51f9d99aa3c4a21b541c4dd09c31c44d53f84b6
cbd <wwwicbd@gmail.com>
2016-05-15 12:58:29 +0800

    junior1

---
+++ Model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cbd
+ * Date: 16/5/15
+ * Time: 上午11:10
+ */
+include "redis-conf.php";
+
+$redis = new Redis();
+$redis->connect($config['redis_local']['host'], $config['redis_local']['post'], 3);
+$redis->select($config['redis_local']['db']);
+
+
+
