c1f7e572f7ab2acd78cd5afd3941a96dd1413340
cbd <wwwicbd@gmail.com>
2016-05-31 16:32:00 +0800

    用户方法demo

--- app/controller/Hello.php
+++ app/controller/Hello.php
@@ -15,4 +15,23 @@ class Hello
 
         include APP_PATH . 'view/Hello.php';
     }
+
+    public function index()
+    {
+        require_once FOCUS_PATH . 'Twig/Autoloader.php';
+        Twig_Autoloader::register();
+
+        $html = file_get_contents(APP_PATH . 'view/index.html');
+        $loader = new Twig_Loader_Array(array(
+            'index' => $html,
+        ));
+        $twig = new Twig_Environment($loader);
+
+
+        $helloDao = new HelloDao();
+        $name = $helloDao->redis->get('name');
+
+
+        echo $twig->render('index', array('name' => $name));
+    }
 }
