b9ac8acb8989fa80a200800f890fd7f1d2a08118
cbd <wwwicbd@gmail.com>
2016-05-31 16:32:59 +0800

    update 路径相关

--- define.php
+++ define.php
@@ -6,14 +6,16 @@
  * Time: 下午6:12
  */
 
-
 //系统路径分隔符
 define('DS', DIRECTORY_SEPARATOR);
-//Focus框架路径
-define("FOCUS_PATH", './focus/');
-//app应用路径
-define("APP_PATH", './app/');
 
+//项目根目录
+define('ROOT_PATH', dirname(__FILE__) . DS);
+
+//Focus框架路径
+define("FOCUS_PATH", ROOT_PATH . 'focus' . DS);
+//app应用路径
+define("APP_PATH", ROOT_PATH . 'app' . DS);
 
 
 //加载框架启动文件
